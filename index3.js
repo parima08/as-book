60fca5ed9772e5c78941e12cfb912292cc169766
Parima Shah <parima@outschool.com>
2021-09-20 19:28:20 -0700

    Fixed horizontal scroll

--- index3.js
+++ index3.js
@@ -1,4 +1,18 @@
 (function () {
+    var swiper = new Swiper('.blog-slider', {
+        spaceBetween: 30,
+        effect: 'fade',
+        loop: true,
+        mousewheel: {
+            invert: false,
+        },
+        // autoHeight: true,
+        pagination: {
+            el: '.blog-slider__pagination',
+            clickable: true,
+        }
+    });
+
     const {
         gsap,
         ScrollTrigger,
@@ -215,10 +229,10 @@
         scrollTrigger: {
             trigger: ".horizontal-scroll-container",
             start: "bottom bottom",
-            end: () => 1000,
+            end: () => 10000,
             scrub: true,
             markers: true,
-            pin: '.horizontal-scroll-window',
+            pin: '.six-fundas',
             invalidateOnRefresh: true,
             anticipatePin: 1,
         }
